48983764708e051367a5b7929011ae5d1f9e7a2b
gusty <gustavodiorlando@gmail.com>
2023-10-12 12:52:36 -0300

    Start item creation module

---
+++ src/itemCreator.js
@@ -0,0 +1,16 @@
+class Item {
+    constructor(title, desc, dueDate, priority, checklist) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checklist = checklist;
+    }
+    check() {
+        if (this.checklist === false) {
+            this.checklist = true;
+        } else {
+            this.checklist = false;
+        }
+    }
+}
